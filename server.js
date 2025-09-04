4827579e18cd4204b2325b3112563e851f9d787a
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-09-04 15:49:29 +0530

    adding frontend for better user Exp.

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
 import { GoogleGenAI } from "@google/genai";
-import express from 'express'
-const app = express();
+import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
-import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 app.use(express.json());
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send("Server is Ready");
-})
 
-const PORT = process.env.PORT;
-const ai = new GoogleGenAI({apiKey: process.env.API_KEY});
+app.use(express.static(path.join(__dirname, "public")));
+
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
 async function main(contents) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: contents,
   });
-  console.log(response.text);
   return response.text;
 }
 
-// await main();
 
-app.get('/api/content', async (req, res) => {
-    try {
-        const data = req.body.question;
-        const result = await main(data);
+app.post("/api/content", async (req, res) => {
+  try {
+    const data = req.body.question;
+    const result = await main(data);
 
-        res.send({
-            "result": result
-        })
-    } catch (error) {
-        res.send("error: "  + error);
-    }
-})
+    res.json({ result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
-app.listen(PORT, ()=> {
-    console.log(`Server is listening on ${PORT}`);
-    
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
 });
